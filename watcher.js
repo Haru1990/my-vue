e43dbd98ddc4e0dad44701ab7a2989206e15fa0e
Haru1990 <Haru1990@users.noreply.github.com>
2019-04-11 16:12:53 +0800

    Create watcher.js
    Create watcher.js

---
+++ watcher.js
@@ -0,0 +1,27 @@
+function Watcher(vm, exp, cb) {
+    this.cb = cb;
+    this.vm = vm;
+    this.exp = exp;
+    // 触发observe的getter将自己添加到订阅器的操作
+    this.value = this.get();
+}
+ 
+Watcher.prototype = {
+    update: function() {
+        this.run();
+    },
+    run: function() {
+        var value = this.vm[this.exp];
+        var oldVal = this.value;
+        if (value !== oldVal) {
+            this.value = value;
+            this.cb.call(this.vm, value, oldVal);
+        }
+    },
+    get: function() {
+        Dep.target = this;
+        var value = this.vm.[this.exp];
+        Dep.target = null;
+        return value;
+    }
+};
