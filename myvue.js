87028eb0cb55b327cba0fe1e561a799c833e8e57
Haru1990 <Haru1990@users.noreply.github.com>
2019-04-12 07:33:05 +0000

    Update myvue.js
    Update myvue.js

--- myvue.js
+++ myvue.js
@@ -1,25 +1,21 @@
-function MyVue (options) {
+function MyVue(options) {
     var self = this;
     var data = this._data = options.data;
     this.$options = options;
  
     Object.keys(data).forEach(function(key) {
-        self._proxy(key);
+        self.proxy(key);
     });
  
+    // 劫持数据的get/set操作 添加订阅者以及变化时通知订阅者更新
     observe(options.data);
     
-    //  在这里手动添加watcher和observe的关系，实际在vue中是通过指令进行关联     
-    // this.$options.el.innerHTML = this.name;
-    // new Watcher(this, 'name', function(value) {
-    //     this.$options.el.innerHTML = value;
-    // });
-    // 使用compile
+    // 使用compile编译dom中的指令 把watcher和observe关联
     new Compile(options.el, this);
 }
 
 // 将对this.xxx的访问代理到this._data.xxx上面来
-MyVue.prototype._proxy = function (key) {
+MyVue.prototype.proxy = function(key) {
     var self = this;
     Object.defineProperty(this, key, {
         enumerable: true,
